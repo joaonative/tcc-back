7ee9d1620a44123073f9712fbe9ade034972c460
joaonative <jestudos127@gmail.com>
2024-03-27 22:27:34 -0300

    feat:  delete event & leave event

--- src/routes/Events.ts
+++ src/routes/Events.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { getEvents, joinEvent } from "../controllers/Event";
+import {
+  createEvent,
+  deleteEvent,
+  getEvents,
+  joinEvent,
+} from "../controllers/Event";
 import { validadeToken } from "../middleware/auth";
 
 const router = express.Router();
 
 router.get("/", validadeToken, getEvents);
-router.get("/join/:id", validadeToken, joinEvent);
+router.post("/", validadeToken, createEvent);
+router.put("/join/:id", validadeToken, joinEvent);
+router.delete("/:id", validadeToken, deleteEvent);
 
 export default router;
