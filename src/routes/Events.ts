04c949446ff423b980a9833e0e2e4494cd46068d
joaonative <jestudos127@gmail.com>
2024-04-30 10:49:58 -0300

    fix: routing declaration

--- src/routes/Events.ts
+++ src/routes/Events.ts
@@ -14,9 +14,9 @@ import { validadeToken } from "../middleware/auth";
 const router = express.Router();
 
 router.get("/", validadeToken, getEvents);
+router.get("/participant", validadeToken, getEventsIsParticipanting);
 router.get("/owner/:id", validadeToken, getEventsByOwner);
 router.get("/:eventId", validadeToken, getEventById);
-router.get("/participant", validadeToken, getEventsIsParticipanting);
 router.post("/", validadeToken, createEvent);
 router.put("/join/:eventId", validadeToken, joinEvent);
 router.get("/leave/:eventId", validadeToken, leaveEvent);
