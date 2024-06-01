d6d118067924b18c0e6647c410c3e4da56985d36
joaonative <jestudos127@gmail.com>
2024-06-01 18:14:40 -0300

    fix search route

--- src/routes/Events.ts
+++ src/routes/Events.ts
@@ -17,6 +17,7 @@ import { validadeToken } from "../middleware/auth";
 const router = express.Router();
 
 router.get("/", validadeToken, getEvents);
+router.get("/search", validadeToken, searchEventsByName);
 router.get("/participant", validadeToken, getEventsIsParticipanting);
 router.get("/owner/:id", validadeToken, getEventsByOwner);
 router.get("/:eventId", validadeToken, getEventById);
@@ -26,6 +27,5 @@ router.get("/community/:communityId", validadeToken, getEventByCommunity);
 router.put("/join/:eventId", validadeToken, joinEvent);
 router.get("/leave/:eventId", validadeToken, leaveEvent);
 router.delete("/:eventId", validadeToken, deleteEvent);
-router.get("/search", validadeToken, searchEventsByName);
 
 export default router;
